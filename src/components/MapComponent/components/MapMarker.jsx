3e0004a7a5b2bc4abf0a58fdcc4ffce727a37155
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2023-03-18 18:25:25 -0500

    incident pins moved to gmaps

--- src/components/MapComponent/components/MapMarker.jsx
+++ src/components/MapComponent/components/MapMarker.jsx
@@ -1,27 +1,25 @@
 import PropTypes from "prop-types";
-import { Icon } from "@iconify/react";
-import MarkerClusterer from '@google/markerclusterer'
+// import MarkerClusterer from '@google/markerclusterer'
 
-export const MapMarkers = (props) => {
-    const { map, maps, markers, onMarkerClick, onMarkerMouseOver, onMarkerMouseOut } = props;
+export const mapMarkers = (props) => {
+    const { mapRef: map, mapsRef, incidents, onMarkerClick } = props;
 
-    const mapMarkers = markers.map((marker) => {
-        const { lat, lng, iconStyle, text, key } = marker;
-        return new maps.Marker({position: {lat, lng}, map, icon: iconStyle, title: text, key});
+    const setupListeners = (marker, incident, onMarkerClick) => {
+        marker.addListener("click", () => {
+            onMarkerClick(incident);
+        });
+        return marker;
+    };
+
+    console.log("mapMarkers: incidents", incidents);
+    const markers = incidents.map((i) => {
+        const { lat, lng, iconStyle, text, key } = i;
+        let newMarker = new mapsRef.Marker({position: {lat, lng}, map, icon: iconStyle, title: text, key});
+
+        setupListeners(newMarker, i, onMarkerClick);
+        return newMarker;
     });
 
-    return mapMarkers;
-
-    // mapMarkers.forEach((marker) => {
-    //     marker.addListener("click", () => {
-    //         onMarkerClick(marker);
-    //     });
-    //     marker.addListener("mouseover", () => {
-    //         onMarkerMouseOver(marker);
-    //     });
-    //     marker.addListener("mouseout", () => {
-    //         onMarkerMouseOut(marker);
-    //     });
-    // });
+    return markers;
 }
 
